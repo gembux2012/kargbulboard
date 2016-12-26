dc0a05f46bc780322b6b6b34a5e011b4922e4cbf
alexc <gembux2012@gmail.com>
2016-12-26 17:17:48 +0700

    r

---
+++ protected/Modules/Paymеnt/Controllers/Index.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Modules\Payment\Controllers;
+
+
+use T4\Mvc\Controller;
+
+class Index
+    extends Controller
+{
+   public function actionDefault()
+   {
+
+   }
+
+   public function actionSetPaid()
+   {
+
+   }
+}
