5228e695016b2a2ed01c8da1d64ea6afd8a6f6a1
alexc <gembux2012@gmail.com>
2016-12-26 15:17:43 +0700

    r

---
+++ protected/Modules/Paymant/Controllers/Index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alexc
+ * Date: 26.12.16
+ * Time: 15:00
+ */
+
+namespace App\Modules\Paymant\Controllers;
+
+
+use App\Components\Admin\Controller;
+
+class Index
+    extends Controller
+{
+   public function actionDefault()
+   {
+
+   }
+}
