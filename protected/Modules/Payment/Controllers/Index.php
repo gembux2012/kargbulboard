1b4afc466fc96511dbf8f9a8e609d286d129151c
alex <gembux2012@gmail.com>
2017-01-21 10:11:56 +0600

    12312

--- protected/Modules/Payment/Controllers/Index.php
+++ protected/Modules/Payment/Controllers/Index.php
@@ -17,8 +17,11 @@ class Index
 
    public function actionSetPaid()
    {
+
       $f = fopen(ROOT_PATH_PUBLIC . DS ."/paid.log","a+");
-      $str = implode(" ",var_dump($this->app->request->post, TRUE));
+      $str = implode(" ",(array(var_export($this->app->request->post, TRUE))));
+      //var_dump($str);die;
+      fputs($f, $str);
       //$item = Story::findByPK($this->app->request->post->label);
       //$item->published = date('Y-m-d H:i:s', time());
      // $item->save();
